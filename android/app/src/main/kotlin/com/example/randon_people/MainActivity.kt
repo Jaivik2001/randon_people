e8b806d05938281521aec7ce197d01b18bc36869
Jaivik <jaivikmoradiya2001@gmail.com>
2023-02-13 09:17:19 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/randon_people/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.randon_people
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:randon_people/screen/HomeScreen.dart';
+
+void main() {
+  runApp(const HomeScreen());
+}
